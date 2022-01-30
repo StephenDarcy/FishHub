6ca9dc6663ef865fad4e787c082a594104a80dcd
Stephen Darcy <stephendarcy123@gmail.com>
2022-01-30 22:33:51 +0000

    another change to cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 var out;
 
 var corsOptions = {
-  origin: "https://fishhub-backend.herokuapp.com",
+  origin: "https://fishhub.tech",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));

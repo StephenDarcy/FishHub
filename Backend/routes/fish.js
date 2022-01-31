e478ddf6de05535e746955d1c4417a0f89b959f6
Stephen Darcy <stephendarcy123@gmail.com>
2022-01-31 00:07:59 +0000

    more api changes

--- Backend/routes/fish.js
+++ Backend/routes/fish.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const R = require("r-script");
 
 // getting scientific name from common
-router.route("scientific/:common").get((req, res) => {
+router.route("/scientific/:common").get((req, res) => {
   var fishName = req.params.common;
   console.log("name received: " + fishName);
   let scientificName = R("./R Scripts/fishbase.R").data(fishName).callSync();

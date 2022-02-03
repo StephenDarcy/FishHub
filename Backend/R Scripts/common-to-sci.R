32b64e0d6550dd67b4645315e6e65de37988f7eb
Stephen Darcy <stephendarcy123@gmail.com>
2022-02-03 17:19:34 +0000

    more changes to R script for heroku testing

--- Backend/R Scripts/common-to-sci.R
+++ Backend/R Scripts/common-to-sci.R
@@ -1,5 +1,4 @@
 # common-to-sci.R
 # used to turn common fishname to scientific
 needs(rfishbase)
-fishNamesTable <- rfishbase::common_to_sci(input)
-fishName <- toString(fishNamesTable[1, 1])
+print(rfishbase::common_to_sci(input))
